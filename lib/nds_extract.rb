cd0b19b9be988403a72f2bce24426aadaa11ca35
Daniel Kersten <dkersten1994@gmail.com>
2020-03-10 12:49:10 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,15 @@
+require 'pp'
+
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
+
+def pretty_print_nds(nds)
+  # Change the code below to pretty print the nds with pp
+  pp nds
+end
+
+# printing directors_database
+# pretty_print_nds(directors_database)
 
 def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
@@ -18,5 +28,23 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
+  
+  result = {}
+  i_director = 0
+  while i_director < nds.length do
+    director = nds[i_director][:name]
+    
+    i_movie = 0
+    result[director] = 0
+    
+    while i_movie < nds[i_director][:movies].length do
+      result[director] += nds[i_director][:movies][i_movie][:worldwide_gross]
+      i_movie += 1
+    end
+    
+    i_director += 1
+  end
+  
+  result
 end
+
